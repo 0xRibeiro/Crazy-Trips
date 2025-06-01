520e31341bdf2e1cf57d8c4857e1d874876b19ca
Victor <142441056+0xRibeiro@users.noreply.github.com>
2025-06-01 12:29:03 -0300

    Initial commit

---
+++ src/main/java/horas/crazytrips/CrazyTripsApplication.java
@@ -0,0 +1,13 @@
+package horas.crazytrips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyTripsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrazyTripsApplication.class, args);
+    }
+
+}
